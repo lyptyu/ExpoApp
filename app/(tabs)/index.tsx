51566d4afe88cdc64697a35159e0e6f310117936
lyptyu <906641851@qq.com>
2025-07-30 14:18:48 +0800

    feat

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,8 @@ import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
 import EmojiList from '@/components/EmojiList';
 import EmojiPicker from '@/components/EmojiPicker';
+import EmojiSticker from '@/components/EmojiSticker';
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 const PlaceholderImage = require('@/assets/images/background-image.png');
 export default function Index() {
     const [selectedImage, setSelectedImage] = useState<string|undefined>(undefined);
@@ -37,27 +39,30 @@ export default function Index() {
     }
     function onSaveImageAsync(){}
     return (
-        <View className={'flex-1 bg-[#25292e] justify-center items-center'}>
+        <GestureHandlerRootView>
+          <View className={'flex-1 bg-[#25292e] justify-center items-center'}>
             <View className="flex-1">
-                <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
+              <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
+              {pickedEmoji && <EmojiSticker imageSize={100} stickerSource={pickedEmoji}/>}
             </View>
             {
-                showAppOptions ? (
-                  <View className={'absolute bottom-[80px]'}>
-                    <View className={'items-center flex-row'}>
-                      <IconButton icon={'refresh'} label={'Reset'} onPress={onReset} />
-                      <CircleButton onPress={onAddSticker} />
-                      <IconButton icon={'save-alt'} label={'Save'} onPress={onSaveImageAsync} />
-                    </View>
+              showAppOptions ? (
+                <View className={'absolute bottom-[80px]'}>
+                  <View className={'items-center flex-row'}>
+                    <IconButton icon={'refresh'} label={'Reset'} onPress={onReset} />
+                    <CircleButton onPress={onAddSticker} />
+                    <IconButton icon={'save-alt'} label={'Save'} onPress={onSaveImageAsync} />
                   </View>
-                ) :(<View className={'basis-[1/3] items-center'}>
-                    <Button label={'Choose a photo'} theme={'primary'} onPress={PickImageAsync}/>
-                    <Button label={'Use this photo'} theme={'secondary'} onPress={()=>setShowAppOptions(true)}/>
-                </View>)
+                </View>
+              ) :(<View className={'basis-[1/3] items-center'}>
+                <Button label={'Choose a photo'} theme={'primary'} onPress={PickImageAsync}/>
+                <Button label={'Use this photo'} theme={'secondary'} onPress={()=>setShowAppOptions(true)}/>
+              </View>)
             }
             <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
               <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
             </EmojiPicker>
-        </View>
+          </View>
+        </GestureHandlerRootView>
     )
 }
