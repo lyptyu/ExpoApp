82520d02db9d33c0f38e43c0705f8fd11ed7e822
lyptyu <906641851@qq.com>
2025-07-30 09:39:31 +0800

    feat

---
+++ components/Button.tsx
@@ -0,0 +1,23 @@
+import { Pressable, View, Text } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+type Props = {
+    label: string;
+    theme: 'primary' | 'secondary';
+}
+export default function Button({ label, theme }: Props) {
+    if (theme === 'primary') {
+        return (
+            <View className="w-[320px] h-[68px] my-[20px] items-center justify-center p-[3px] border-[4px] border-[#ffd33d] rounded-[18px]">
+                <Pressable className="rounded-[10px] w-full h-full items-center justify-center flex-row bg-[#fff]"
+                    onPress={() => {
+                        alert('pressed');
+                    }}
+                >
+                    <FontAwesome name="picture-o" size={18} color="#25292e" className="pr-[8px]" />
+                    <Text className="text-[#25292e] text-[16px]">{label}</Text>
+                </Pressable>
+            </View>
+        )
+    }
+}
--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -4,5 +4,5 @@ type Props = {
   imgSource: ImageSourcePropType;
 };
 export default function ImageViewer({ imgSource }: Props) {
-    return <Image source={imgSource} className="w-[320] h-[440] rounded-[18]" />
+    return <Image source={imgSource} className="w-[320px] h-[440px] rounded-[18px]" />
 }
