83830aae546c35b69b9c2f9f856f5c00efaebe93
linyipeng <lyptyu@vip.qq.com>
2025-07-31 09:46:03 +0800

    refactor: 移除未使用的组件并简化页面结构
    删除多个未使用的React Native组件（IconButton、CircleButton等）并简化主页和关于页面的实现

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -2,21 +2,8 @@ import { View, StyleSheet, Text } from "react-native";
 
 export default function AboutScreen() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>
-                这是关于页面
-            </Text>
+        <View >
+           About
         </View>
     )
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#25292e',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    text: {
-        color: '#fff'
-    }
-})
